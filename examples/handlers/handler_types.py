2af79e2d7dd70955e1ec11fd034f1f048dfc59ff
thethirdfloordev <31870461+thethirdfloordev@users.noreply.github.com>
2021-08-02 09:43:52 -0700

    handlers updated

--- examples/handlers/handler_types.py
+++ examples/handlers/handler_types.py
@@ -35,8 +35,11 @@ def rotating_file_handler(log_location):
 
 #TimedRotatingFileHandler - send logs to disk on a timed interval
 def timed_rotating_file_handler(log_location):
-    from logging.handlers import RotatingFileHandler
-    hdlr = RotatingFileHandler(log_location, maxBytes=2000, backupCount=10)
+    from logging.handlers import TimedRotatingFileHandler
+    from datetime import time
+    rollover_time = time(11, 34, second=55)
+    hdlr = TimedRotatingFileHandler(log_location, when="H", backupCount=10, atTime=rollover_time)
+
     log = logging.getLogger("shared")
     log.addHandler(hdlr)
 
@@ -49,5 +52,5 @@ def send_a_thousand(log):
 if __name__ == "__main__":
     # stream_handler(sys.stderr)
     # file_handler("logs/file_handling")
-    rotating_file_handler("logs/rotation/rotation_file_handling")
-    rotating_file_handler("logs/rotation/timed/timed_file_handling")
+    # rotating_file_handler("logs/rotation/rotation_file_handling")
+    timed_rotating_file_handler("logs/rotation/timed/timed_file_handling")
